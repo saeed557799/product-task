b35d05ddc147c032f7bdb3c725f18b372b75b45a
Umair <umairqadeer.work@gmail.com>
2023-11-17 12:04:09 +0500

    implement persist store

--- src/redux/reducers/rootReducer.js
+++ src/redux/reducers/rootReducer.js
@@ -2,12 +2,13 @@ import auth from './duck/authDuck';
 import content from './duck/contentDuck';
 import quiz from './duck/quizDuck';
 import dashboard from './duck/dashboardDuck';
+import { combineReducers } from '@reduxjs/toolkit';
 
-const rootReducers = {
+const rootReducers = combineReducers({
   auth,
   content,
   quiz,
   dashboard,
-};
+});
 
 export default rootReducers;
